63d48f7a0966186a8cc2ee7644778948b071482f
Dinopez <153353882+Dinopez@users.noreply.github.com>
2025-08-08 20:00:37 +0700

    Update myscript.lua

--- myscript.lua
+++ myscript.lua
@@ -1,2 +1,7 @@
-print("Hello từ GitHub!")
-game.Players.LocalPlayer:Kick("Script đã chạy thành công!")
+print("Script đang chạy, chờ 3 giây...")
+wait(3)
+game.StarterGui:SetCore("SendNotification", {
+    Title = "Thông báo";
+    Text = "Script từ GitHub đã chạy xong!";
+    Duration = 5;
+})
